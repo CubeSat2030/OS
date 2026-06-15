3b0b9cdc5c4e7f68099d4a1dbc3ada09d2a88fed
Nathan-Busse <bussenathan@icloud.com>
2026-06-15 14:52:26 +0200

    Update init_os.c binary version and size; remove unused files

--- Core/src/rootfs/init_os.c
+++ Core/src/rootfs/init_os.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd949aebdbfd5a2e7f228b328ff4af314b75135e16bf4703c2a2c1224f95e86a
-size 8603
+oid sha256:d2ccb929ce3ded30e9839e91e5bfd0175630dd8b5b0d8af7a1c7fc6bbf957ddf
+size 12148
